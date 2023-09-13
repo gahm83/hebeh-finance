32f18c943f853f2c466c7da558e7df436c688065
Farid García <farid.herrera@gmail.com>
2023-09-13 17:34:12 -0600

    Form fields validation

--- app/hooks/useConfirm.js
+++ app/hooks/useConfirm.js
@@ -27,27 +27,32 @@ const useConfirm = (title, message) => {
   };
 
   const onChange = async () => {
-
     const token = captchaRef?.current?.getValue();
     const response = await fetch("/api/validatecaptcha", {
       method: 'POST',
       body: JSON.stringify({ token }),
     });
-    
-    console.log(response);
-    
+    const data = await response.json();
+    if (data?.data?.success) {
+      handleConfirm();
+    } else {
+      handleCancel();
+    }
   }
 
   const ConfirmationDialog = () => (
-    <section className={`fixed ${promise === null ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto'} bg-[#051b38] bg-opacity-95 text-white w-screen h-screen z-20`}>
-      <ReCAPTCHA
-        sitekey={process.env.RECAPTCHA_SITE_KEY}
-        onChange={onChange}
-        ref={captchaRef}
-      />
-      <div className="flex space-x-3">
-        <button onClick={handleConfirm}>Yes</button>
-        <button onClick={handleCancel}>Cancel</button>
+    <section className={`fixed ${promise === null ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto'} bg-[#051b38] bg-opacity-95 flex items-center justify-center text-white w-screen h-screen z-20`}>
+      <div>
+
+        <ReCAPTCHA
+          sitekey={process.env.RECAPTCHA_SITE_KEY}
+          onChange={onChange}
+          ref={captchaRef}
+        />
+        <div className="flex space-x-3">
+          <button onClick={handleConfirm}>Yes</button>
+          <button onClick={handleCancel}>Cancel</button>
+        </div>
       </div>
     </section>
   );
