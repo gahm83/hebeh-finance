8b0f171705e435d448adb901d65fc1603c231245
Farid García <farid.herrera@gmail.com>
2023-09-12 22:10:23 -0600

    First commit with onConfirm hook

--- app/components/simuladordepagos.js
+++ app/components/simuladordepagos.js
@@ -7,11 +7,16 @@ import formData from 'form-data';
 import Mailgun from 'mailgun.js';
 import toast, { Toaster } from 'react-hot-toast';
 
+import useConfirm from '../hooks/useConfirm';
 import Ornament from './ornament/ornament';
 
 export default function Simulador() {
 
   const [planDePagos, setPlanDePagos] = useState([]);
+  const [Dialog, notARobot] = useConfirm(
+    'Are you sure?',
+    'Are you sure you want to delete user "Isaac Kwok"?',
+  )
   //const [isToast, setToast] = useState(false);
 
   const mailgun = new Mailgun(formData);
@@ -63,23 +68,25 @@ export default function Simulador() {
   const onSubmit = async (data, event) => {
     event.preventDefault();
     console.log(data);
+    const reCaptcha = await notARobot();
+    console.log(reCaptcha)
     // const { list, to, subject, template, message } = data;
     // const sendTo = toFieldVisible ? to : list;
 
     // setToast(true);
 
-    const sendMessage = mg.messages.create(process.env.MAILGUN_DOMAIN, {
-      from: "Farid García <farid.herrera@gmail.com>",
-      to: "farid.herrera@gmail.com",
-      subject: "Contacto desde el Simulador de Pagos",
-      text: "Aquí va una prueba",
-    }).then(msg => {
-        reset();
-        // setToast(false);
-    }).catch(err => {
-      console.error(err)
-      // setToast(false);
-    });
+    // const sendMessage = mg.messages.create(process.env.MAILGUN_DOMAIN, {
+    //   from: "Farid García <farid.herrera@gmail.com>",
+    //   to: "farid.herrera@gmail.com",
+    //   subject: "Contacto desde el Simulador de Pagos",
+    //   text: "Aquí va una prueba",
+    // }).then(msg => {
+    //     reset();
+    //     // setToast(false);
+    // }).catch(err => {
+    //   console.error(err)
+    //   // setToast(false);
+    // });
 
     // toast.promise(
     //   sendMessage,
@@ -105,81 +112,84 @@ export default function Simulador() {
   };
 
   return (
-    <section className="relative bg-[#051b38] text-white z-10">
-      <Ornament className="opacity-40" />
-      <div className="pt-12 pb-32 md:py-24 px-6 relative">
-        <div className="xl:w-5/12 mx-auto">
-          <h1 className="text-3xl md:text-5xl font-bold text-center uppercase mb-4 md:mb-8">Simulador de pagos de tarjeta de crédito</h1>
+    <>
+      <section className="relative bg-[#051b38] text-white">
+        <Ornament className="small opacity-40" />
+        <div className="relative pt-12 pb-32 md:py-24 px-6 z-10">
+          <div className="xl:w-5/12 mx-auto">
+            <h1 className="text-3xl md:text-5xl font-bold text-center uppercase mb-4 md:mb-8">Simulador de pagos de tarjeta de crédito</h1>
+          </div>
+          <form
+            className="flex flex-col space-y-6 lg:w-3/4 2xl:w-7/12 mx-auto"
+            onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid grid-cols-12 md:flex md:items-center md:justify-center gap-y-3">
+              <label className="col-span-12 text-center md:mr-3" htmlFor="adeudo">Total del adeudo en TDC:</label>
+              <div className="col-span-8 md:w-56">
+                <input className="rounded-r-none text-black" {...register("adeudo")} />
+              </div>
+              <div className="col-span-4">
+                <a className="button rounded-l-none md:px-6" onClick={() => handleCalculation()}><span>Calcular</span></a>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-12 gap-6">
+              <div className="md:col-span-7 grid grid-cols-10 gap-1 md:gap-3">
+                <div className="bg-slate-200 text-black col-span-3 flex flex-col justify-center text-center py-3 md:py-0 rounded-l-md">
+                  <h3 className="font-bold leading-tight px-4 md:px-6">Plazos en meses</h3>
+                  {
+                    (planDePagos && planDePagos.length !== 0) &&
+                    <div className="flex flex-col mt-3">
+                    {
+                      planDePagos.map(item => <span key={item.mensualidades}>{item.mensualidades}</span>)
+                    }
+                    </div>
+                  }
+                </div>
+                <div className="bg-slate-200 text-black col-span-4 flex flex-col justify-center text-center py-3 md:py-0">
+                  <h3 className="font-bold leading-tight px-6">Cuota mensual con HEBEH</h3>
+                  {
+                    (planDePagos && planDePagos.length !== 0) &&
+                    <div className="flex flex-col mt-3">
+                    {
+                      planDePagos.map(item => <span key={item.mensualidades}>{`$${item.cuota}`}</span>)
+                    }
+                    </div>
+                  }
+                </div>
+                <div className="bg-slate-200 text-black col-span-3 flex flex-col justify-center text-center py-3 md:py-0 rounded-r-md">
+                  <h3 className="font-bold leading-tight px-4 md:px-6">Tasa de Interés</h3>
+                  {
+                    (planDePagos && planDePagos.length !== 0) &&
+                    <div className="flex flex-col mt-3">
+                    {
+                      planDePagos.map(item => <span key={item.tasa}>{`${item.tasa}%`}</span>)
+                    }
+                    </div>
+                  }
+                </div>
+              </div>
+              <div className="md:col-span-5">
+                <h3 className="text-xl md:text-2xl font-bold text-center mb-3">Mándanos tus datos para sanar tu cuenta bancaria</h3>
+                <fieldset className="grid grid-cols-12 gap-4">
+                  <div className="col-span-12">
+                    <input {...register("nombre")} placeholder="Nombre completo" className="w-full"/>
+                  </div>
+                  <div className="col-span-12">
+                    <input {...register("email")} placeholder="Correo electrónico" className="w-full"/>
+                  </div>
+                  <div className="col-span-8">
+                    <input {...register("telefono")} placeholder="Teléfono" className="w-full" />
+                  </div>
+                  <div className="col-span-4">
+                    <button disabled={!isDirty || !isValid} className="justify-center w-full">Enviar</button>
+                  </div>
+                  
+                </fieldset>
+              </div>
+            </div>
+          </form>
         </div>
-        <form
-          className="flex flex-col space-y-6 lg:w-3/4 2xl:w-7/12 mx-auto"
-          onSubmit={handleSubmit(onSubmit)}>
-          <div className="grid grid-cols-12 md:flex md:items-center md:justify-center gap-y-3">
-            <label className="col-span-12 text-center md:mr-3" htmlFor="adeudo">Total del adeudo en TDC:</label>
-            <div className="col-span-8 md:w-56">
-              <input className="rounded-r-none text-black" {...register("adeudo")} />
-            </div>
-            <div className="col-span-4">
-              <a className="button rounded-l-none md:px-6" onClick={() => handleCalculation()}><span>Calcular</span></a>
-            </div>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-12 gap-6">
-            <div className="md:col-span-7 grid grid-cols-10 gap-1 md:gap-3">
-              <div className="bg-slate-200 text-black col-span-3 flex flex-col justify-center text-center py-3 md:py-0 rounded-l-md">
-                <h3 className="font-bold leading-tight px-4 md:px-6">Plazos en meses</h3>
-                {
-                  (planDePagos && planDePagos.length !== 0) &&
-                  <div className="flex flex-col mt-3">
-                  {
-                    planDePagos.map(item => <span key={item.mensualidades}>{item.mensualidades}</span>)
-                  }
-                  </div>
-                }
-              </div>
-              <div className="bg-slate-200 text-black col-span-4 flex flex-col justify-center text-center py-3 md:py-0">
-                <h3 className="font-bold leading-tight px-6">Cuota mensual con HEBEH</h3>
-                {
-                  (planDePagos && planDePagos.length !== 0) &&
-                  <div className="flex flex-col mt-3">
-                  {
-                    planDePagos.map(item => <span key={item.mensualidades}>{`$${item.cuota}`}</span>)
-                  }
-                  </div>
-                }
-              </div>
-              <div className="bg-slate-200 text-black col-span-3 flex flex-col justify-center text-center py-3 md:py-0 rounded-r-md">
-                <h3 className="font-bold leading-tight px-4 md:px-6">Tasa de Interés</h3>
-                {
-                  (planDePagos && planDePagos.length !== 0) &&
-                  <div className="flex flex-col mt-3">
-                  {
-                    planDePagos.map(item => <span key={item.tasa}>{`${item.tasa}%`}</span>)
-                  }
-                  </div>
-                }
-              </div>
-            </div>
-            <div className="md:col-span-5">
-              <h3 className="text-xl md:text-2xl font-bold text-center mb-3">Mándanos tus datos para sanar tu cuenta bancaria</h3>
-              <fieldset className="grid grid-cols-12 gap-4">
-                <div className="col-span-12">
-                  <input {...register("nombre")} placeholder="Nombre completo" className="w-full"/>
-                </div>
-                <div className="col-span-12">
-                  <input {...register("email")} placeholder="Correo electrónico" className="w-full"/>
-                </div>
-                <div className="col-span-8">
-                  <input {...register("telefono")} placeholder="Teléfono" className="w-full" />
-                </div>
-                <div className="col-span-4">
-                  <button disabled={!isDirty || !isValid} className="justify-center w-full">Enviar</button>
-                </div>
-                
-              </fieldset>
-            </div>
-          </div>
-        </form>
-      </div>
-    </section>
+      </section>
+      <Dialog />
+    </>
   )
 }
--- app/hooks/useConfirm.js
+++ app/hooks/useConfirm.js
@@ -1,10 +1,14 @@
 'use client'
-import { useState } from "react";
-import classNames from "classnames";
-// import ReCAPTCHA from "react-google-recaptcha";
+import { useState, useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const useConfirm = (title, message) => {
+
+  
   const [promise, setPromise] = useState(null);
+  const captchaRef = useRef(null);
+  const token = captchaRef?.current?.getValue();
+  
   const confirm = () => new Promise((resolve, reject) => {
     setPromise({ resolve });
   });
@@ -23,16 +27,17 @@ const useConfirm = (title, message) => {
     handleClose();
   };
 
+  const onChange = () => {
+    console.log()
+  }
+
   const ConfirmationDialog = () => (
     <section className={`fixed ${promise === null ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto'} bg-[#051b38] bg-opacity-95 text-white w-screen h-screen z-20`}>
-    {/* <ReCAPTCHA
-          style={{ display: "inline-block" }}
-          theme="dark"
-          ref={this._reCaptchaRef}
-          sitekey={process.env.REACT_APP_SITE_KEY}
-          onChange={this.handleChange}
-          // asyncScriptOnLoad={this.asyncScriptOnLoad}
-        /> */}
+      {/* <ReCAPTCHA /> */}
+      <div className="flex space-x-3">
+        <button onClick={handleConfirm}>Yes</button>
+        <button onClick={handleCancel}>Cancel</button>
+      </div>
     </section>
   );
   return [ConfirmationDialog, confirm];
