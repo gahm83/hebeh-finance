9a4806bfca33f4fcb1b93e1460171ee1e52bc946
Farid García <farid.herrera@gmail.com>
2023-09-13 09:51:00 -0600

    Captcha implementation, verify pending

--- app/hooks/useConfirm.js
+++ app/hooks/useConfirm.js
@@ -7,7 +7,6 @@ const useConfirm = (title, message) => {
   
   const [promise, setPromise] = useState(null);
   const captchaRef = useRef(null);
-  const token = captchaRef?.current?.getValue();
   
   const confirm = () => new Promise((resolve, reject) => {
     setPromise({ resolve });
@@ -27,13 +26,55 @@ const useConfirm = (title, message) => {
     handleClose();
   };
 
-  const onChange = () => {
-    console.log()
+  const onChange = async () => {
+    const token = captchaRef?.current?.getValue();
+    try {
+      // Ping the google recaptcha verify API to verify the captcha code you received
+      const response = await fetch(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+            'Access-Control-Allow-Origin': '127.0.0.0'
+          },
+        }
+      );
+      const captchaValidation = await response.json();
+      /**
+       * The structure of response from the veirfy API is
+       * {
+       *  "success": true|false,
+       *  "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+       *  "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+       *  "error-codes": [...]        // optional
+        }
+       */
+      if (captchaValidation.success) {
+        // Replace this with the API that will save the data received
+        // to your backend
+        // await sleep();
+        // Return 200 if everything is successful
+        // return res.status(200).send("OK");
+        handleConfirm();
+      }
+
+      // return res.status(422).json({
+      //   message: "Unproccesable request, Invalid captcha code",
+      // });
+    } catch (error) {
+      console.log(error);
+      // return res.status(422).json({ message: "Something went wrong" });
+    }
   }
 
   const ConfirmationDialog = () => (
     <section className={`fixed ${promise === null ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto'} bg-[#051b38] bg-opacity-95 text-white w-screen h-screen z-20`}>
-      {/* <ReCAPTCHA /> */}
+      <ReCAPTCHA
+        sitekey={process.env.RECAPTCHA_SITE_KEY}
+        onChange={onChange}
+        ref={captchaRef}
+      />
       <div className="flex space-x-3">
         <button onClick={handleConfirm}>Yes</button>
         <button onClick={handleCancel}>Cancel</button>
--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   env: {
     MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
     MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN,
+    RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
+    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
   },
 }
 
